6a240626ba47c7fac8923f346c8c27ae8d38ad3d
takeyu1013 <yuto1013@gmail.com>
2022-07-30 23:13:20 +0900

    🚧

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import { SignedIn, useUser } from "@clerk/nextjs";
 import type { NextPage } from "next";
+import type { Post } from "@prisma/client";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div>foo</div>;
+  const { user } = useUser();
+  const primaryEmailAddress = user?.primaryEmailAddress?.emailAddress;
+  console.log(primaryEmailAddress);
+
+  const post: Post = {
+    id: "foo",
+    author: "foo",
+    authorEmail: "foo@example.com",
+    body: "bar",
+    createdAt: new Date(Date.now()),
+    title: "baz",
+    views: 0,
+  };
+
+  return (
+    <SignedIn>
+      {primaryEmailAddress === post.authorEmail && (
+        <Image onClick={async () => {}} alt="delete" src="vercel.svg" />
+      )}
+    </SignedIn>
+  );
 };
 
 export default Home;
