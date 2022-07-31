b3f5129fe6e0c14629858528747a4cd22002e23c
takeyu1013 <yuto1013@gmail.com>
2022-07-31 12:46:20 +0900

    ✨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { SignedIn, useUser } from "@clerk/nextjs";
+import { SignedIn, useClerk, useUser } from "@clerk/nextjs";
 import type { NextPage } from "next";
 import type { Post } from "@prisma/client";
 import Image from "next/image";
 
 const Home: NextPage = () => {
+  const { openSignIn } = useClerk();
   const { user } = useUser();
   const primaryEmailAddress = user?.primaryEmailAddress?.emailAddress;
   console.log(primaryEmailAddress);
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
   const post: Post = {
     id: "foo",
     author: "foo",
-    authorEmail: "foo@example.com",
+    authorEmail: process.env.NEXT_PUBLIC_EMAIL || "",
     body: "bar",
     createdAt: new Date(Date.now()),
     title: "baz",
@@ -19,11 +20,26 @@ const Home: NextPage = () => {
   };
 
   return (
-    <SignedIn>
-      {primaryEmailAddress === post.authorEmail && (
-        <Image onClick={async () => {}} alt="delete" src="vercel.svg" />
-      )}
-    </SignedIn>
+    <main>
+      <SignedIn>
+        {primaryEmailAddress === post.authorEmail && (
+          <Image
+            onClick={async () => {}}
+            alt="delete"
+            src="/vercel.svg"
+            width={100}
+            height={100}
+          />
+        )}
+      </SignedIn>
+      <button
+        onClick={() => {
+          openSignIn();
+        }}
+      >
+        Sign In
+      </button>
+    </main>
   );
 };
 
